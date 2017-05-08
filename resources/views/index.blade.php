615fbccc3e6e4006082a4bfebaccafbaadeeac97
Kyle Anderson <kyle@divspace.com>
2017-05-08 15:55:13 -0700

    Added favicons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,14 @@
         <title>{{ config('app.name') }}</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css" charset="utf-8">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" charset="utf-8">
+        <link href="{{ asset('apple-touch-icon.png') }}" rel="apple-touch-icon" sizes="180x180">
+        <link href="{{ asset('favicon-32x32.png') }}" rel="icon" type="image/png" sizes="32x32">
+        <link href="{{ asset('favicon-16x16.png') }}" rel="icon" type="image/png" sizes="16x16">
+        <link href="{{ asset('manifest.json') }}" rel="manifest">
+        <link href="{{ asset('safari-pinned-tab.svg') }}" rel="mask-icon" color="#5bbad5">
+        <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
+        <meta name="application-name" content="{{ config('app.name') }}">
+        <meta name="theme-color" content="#ffffff">
     </head>
     <body>
         <header>
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,6 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .copy('resources/assets/cv/resume.pdf', 'public/')
+   .copy('resources/assets/cv/resume.pdf', 'public')
+   .copyDirectory('resources/assets/favicon', 'public')
    .copyDirectory('resources/assets/img', 'public/img');

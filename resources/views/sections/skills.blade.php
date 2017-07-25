1608d58456fa970b18a4a51d1b9b03f58a93b94f
Kyle Anderson <kyle@divspace.com>
2017-07-25 16:55:09 -0700

    Added additional skills

--- resources/views/sections/skills.blade.php
+++ resources/views/sections/skills.blade.php
@@ -23,30 +23,25 @@
     <div class="other-skills">
         <h3 class="subtitle">Other Skills</h3>
         <div class="misc-skills">
-            <span class="skill-tag">Bash</span>
-            <span class="skill-tag">Bower</span>
-            <span class="skill-tag">Git</span>
-            <span class="skill-tag">Gulp</span>
-            <br>
-            <span class="skill-tag">MySQL</span>
-            <span class="skill-tag">Perl</span>
-            <span class="skill-tag">SCSS</span>
-            <span class="skill-tag">WordPress</span>
-        </div>
-    </div>
-    <div class="other-skills">
-        <h3 class="subtitle">Knowledgeable In</h3>
-        <div class="misc-skills">
+            <span class="skill-tag">Amazon Web Services</span>
             <span class="skill-tag">Apache</span>
             <span class="skill-tag">Backbone</span>
+            <span class="skill-tag">Bash</span>
             <span class="skill-tag">CodeIgniter</span>
             <span class="skill-tag">cPanel / WHM</span>
-            <br>
-            <span class="skill-tag">ExpressionEngine</span>
+            <span class="skill-tag">Git</span>
+            <span class="skill-tag">Gulp</span>
             <span class="skill-tag">Magento</span>
+            <span class="skill-tag">MySQL</span>
             <span class="skill-tag">Nginx</span>
+            <span class="skill-tag">Perl</span>
+            <span class="skill-tag">Roots / Bedrock</span>
+            <span class="skill-tag">Sass / SCSS</span>
             <span class="skill-tag">Scrum</span>
-            <span class="skill-tag">Vue</span>
+            <span class="skill-tag">Vagrant</span>
+            <span class="skill-tag">VueJS</span>
+            <span class="skill-tag">Webpack</span>
+            <span class="skill-tag">WordPress</span>
         </div>
     </div>
 </section>

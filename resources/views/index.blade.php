3d601c9eeced9e104dc6f78e26f5745dfad0c205
Kyle Anderson <kyle@divspace.com>
2017-07-25 17:02:01 -0700

    Updated page title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>{{ config('app.name') }}</title>
+        <title>Full Stack PHP Developer | Kyle Anderson</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic" rel="stylesheet">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         @include('partials.favicons')

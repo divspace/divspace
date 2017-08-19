99ce967877708d3af86e52d12b63088af5fbc0c4
Kyle Anderson <kyle@divspace.com>
2017-08-18 18:45:27 -0700

    Add time elements with datetime attribute (YYYY-MM-DD)

--- resources/views/sections/experience.blade.php
+++ resources/views/sections/experience.blade.php
@@ -3,6 +3,21 @@
     <div class="timeline">
         <div class="item">
             <div class="work-place">
+                <h3 class="place">Apria Healthcare</h3>
+                <div class="location">
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>Chandler, AZ
+                </div>
+            </div>
+            <div class="job-meta">
+                <div class="title">Senior Software Engineer</div>
+                <div class="time">
+                    <time datetime="2017-08-21">August 2017</time> &ndash; <time datetime="{{ date('Y-m-d') }}">Present</time>
+                </div>
+            </div>
+            <div class="job-desc"></div>
+        </div>
+        <div class="item">
+            <div class="work-place">
                 <h3 class="place">Privoro</h3>
                 <div class="location">
                     <i class="fa fa-map-marker" aria-hidden="true"></i>Tempe, AZ
@@ -10,14 +25,16 @@
             </div>
             <div class="job-meta">
                 <div class="title">PHP Programmer</div>
-                <div class="time">July 2016 &ndash; April 2017</div>
+                <div class="time">
+                    <time datetime="2016-07-18">July 2016</time> &ndash; <time datetime="2017-04-28">April 2017</time>
+                </div>
             </div>
             <div class="job-desc">
                 <ul>
-                    <li>Increased security and reduced development time by converting existing website to Laravel</li>
-                    <li>Reduced page load time from 6.9 to 2.1 seconds by switching the server stack from Apache, MySQL, and PHP 5.4 on HTTP/1.1 to Nginx, MariaDB, and PHP 7.1 on HTTP/2</li>
-                    <li>Provided instant updates and easier management for non-systems administrators by adding Laravel Forge for server management and Laravel Envoyer for zero downtime deployment</li>
-                    <li>Reduced server and support costs by migrating from Amazon Web Services (AWS) to DigitalOcean</li>
+                    <li>Increased security and reduced development time by converting existing website to Laravel.</li>
+                    <li>Reduced page load time from 6.9 to 2.1 seconds by switching the server stack from Apache, MySQL, and PHP 5.4 on HTTP/1.1 to Nginx, MariaDB, and PHP 7.1 on HTTP/2.</li>
+                    <li>Provided instant updates and easier management for non-systems administrators by adding Laravel Forge for server management and Laravel Envoyer for zero downtime deployment.</li>
+                    <li>Reduced server and support costs by migrating from Amazon Web Services (AWS) to DigitalOcean.</li>
                 </ul>
             </div>
         </div>
@@ -30,12 +47,14 @@
             </div>
             <div class="job-meta">
                 <div class="title">Web Developer</div>
-                <div class="time">January 2016 &ndash; May 2016</div>
+                <div class="time">
+                    <time datetime="2016-01-04">January 2016</time> &ndash; <time datetime="2016-05-23">May 2016</time>
+                </div>
             </div>
             <div class="job-desc">
                 <ul>
-                    <li>Reduced initial setup time for new projects from 3-5 days to 4 hours by creating a custom WordPress boilerplate framework, theme, and auto-deploying virtual environment</li>
-                    <li>Created the development and deployment process by adding task automation, unit and style testing, and Continuous Integration (CI)</li>
+                    <li>Reduced initial setup time for new projects from 3-5 days to 4 hours by creating a custom WordPress boilerplate framework, theme, and auto-deploying virtual environment.</li>
+                    <li>Created the development and deployment process by adding task automation, unit and style testing, and Continuous Integration (CI).</li>
                 </ul>
             </div>
         </div>
@@ -48,12 +67,14 @@
             </div>
             <div class="job-meta">
                 <div class="title">Programmer</div>
-                <div class="time">July 2015 &ndash; November 2015</div>
+                <div class="time">
+                    <time datetime="2015-07-06">July 2015</time> &ndash; <time datetime="2015-11-27">November 2015</time>
+                </div>
             </div>
             <div class="job-desc">
                 <ul>
-                    <li>Increased productivity for existing development team by introducing Gulp for task automation, Bower for managing JavaScript libraries, and Sass for improved readability and organization for CSS</li>
-                    <li>Reduced development time and errors by refactoring the existing codebase using the DRY principle, creating repositories, transformers, presenters, decorators, and view composers</li>
+                    <li>Increased productivity for existing development team by introducing Gulp for task automation, Bower for managing JavaScript libraries, and Sass for improved readability and organization for CSS.</li>
+                    <li>Reduced development time and errors by refactoring the existing codebase using the DRY principle, creating repositories, transformers, presenters, decorators, and view composers.</li>
                 </ul>
             </div>
         </div>
@@ -66,12 +87,14 @@
             </div>
             <div class="job-meta">
                 <div class="title">Chief Technology Officer</div>
-                <div class="time">September 2014 &ndash; April 2015</div>
+                <div class="time">
+                    <time datetime="2014-09-05">September 2014</time> &ndash; <time datetime="2014-04-30">April 2015</time>
+                </div>
             </div>
             <div class="job-desc">
                 <ul>
-                    <li>Designed the web hosting department's technical infrastructure using Amazon Web Services (AWS), implementing server hardening and security and identifying a billing / payment processing solution</li>
-                    <li>Established a social media presence and high-ROI digital marketing strategy, including PPC, SEM, SMM, SEO, and email campaign, averaging a 28% open rate and 16% click-through rate</li>
+                    <li>Designed the web hosting department's technical infrastructure using Amazon Web Services (AWS), implementing server hardening and security and identifying a billing / payment processing solution.</li>
+                    <li>Established a social media presence and high-ROI digital marketing strategy, including PPC, SEM, SMM, SEO, and email campaign, averaging a 28% open rate and 16% click-through rate.</li>
                 </ul>
             </div>
         </div>
@@ -84,14 +107,16 @@
             </div>
             <div class="job-meta">
                 <div class="title">Co-Founder</div>
-                <div class="time">May 2006 &ndash; August 2014</div>
+                <div class="time">
+                    <time datetime="2006-05-22">May 2006</time> &ndash; <time datetime="2017-08-05">August 2014</time>
+                </div>
             </div>
             <div class="job-desc">
                 <ul>
-                    <li>Established and managed daily business aspects, from infrastructure development and maintenance to customer acquisition, web design and development, and technical support</li>
-                    <li>Grew Crucial into $1MM+ in annual revenues through targeted marketing, consistent infrastructure modernization, and providing outstanding user experiences</li>
-                    <li>Wrote blog and knowledgebase articles to help clients and increase online presence, earning prominent placement in search and industry publications, such as <em>Smashing Magazine</em> and <em>A List Apart</em></li>
-                    <li>Established Crucial as the industry's most prominent hosting provider for Magento, serving as a technical product expert and benchmark for performance and optimization</li>
+                    <li>Established and managed daily business aspects, from infrastructure development and maintenance to customer acquisition, web design and development, and technical support.</li>
+                    <li>Grew Crucial into $1MM+ in annual revenues through targeted marketing, consistent infrastructure modernization, and providing outstanding user experiences.</li>
+                    <li>Wrote blog and knowledgebase articles to help clients and increase online presence, earning prominent placement in search and industry publications, such as <em>Smashing Magazine</em> and <em>A List Apart</em>.</li>
+                    <li>Established Crucial as the industry's most prominent hosting provider for Magento, serving as a technical product expert and benchmark for performance and optimization.</li>
                 </ul>
             </div>
         </div>
@@ -104,13 +129,15 @@
             </div>
             <div class="job-meta">
                 <div class="title">IT Manager</div>
-                <div class="time">August 2001 &ndash; May 2002</div>
+                <div class="time">
+                    <time datetime="2001-08-22">August 2001</time> &ndash; <time datetime="2002-05-22">May 2002</time>
+                </div>
             </div>
             <div class="job-desc">
                 <ul>
-                    <li>Increased efficiency for the creative department by developing an online presence that allowed clients to upload media files directly via the website</li>
-                    <li>Reduced time spent from the accounting department by allowing employees in satellite offices to connect directly to the Phoenix office via VPN to clock in and out and directly access the mainframe</li>
-                    <li>Provided general technical support, such as wiring buildings with CAT5, rebuilding computers, and fixing common software and hardware issues</li>
+                    <li>Increased efficiency for the creative department by developing an online presence that allowed clients to upload media files directly via the website.</li>
+                    <li>Reduced time spent from the accounting department by allowing employees in satellite offices to connect directly to the Phoenix office via VPN to clock in and out and directly access the mainframe.</li>
+                    <li>Provided general technical support, such as wiring buildings with CAT5, rebuilding computers, and fixing common software and hardware issues.</li>
                 </ul>
             </div>
         </div>
